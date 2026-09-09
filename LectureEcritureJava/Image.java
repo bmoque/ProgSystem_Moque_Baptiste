ad6c04b9dcc7211cba6dff514e1f0d6dce02f25e
bmoque <baptiste.moque@iut-rodez.fr>
2026-09-09 16:29:51 +0200

    Création du dégradé bleu

--- LectureEcritureJava/Image.java
+++ LectureEcritureJava/Image.java
@@ -1,4 +1,5 @@
 import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.IOException;
 
 public class Image {
@@ -13,17 +14,17 @@ public class Image {
     /**
      * Constructeur : initialise une image vide.
      */
-    public Image(int width, int hauteur) {
+    public Image(int width, int height) {
         this.width = width;
         this.height = height;
-        pixels = new int[hauteur][largeur][3];
+        pixels = new int[height][width][3];
     }
 
     /**
      * Définit la couleur d'un pixel à la position (x, y)
      */
     public void setPixel(int x, int y, int r, int g, int b) {
-        if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
             pixels[y][x][0] = r;
             pixels[y][x][1] = g;
             pixels[y][x][2] = b;
@@ -34,6 +35,19 @@ public class Image {
      * Sauvegarde l'image au format texte PPM (P3)
      */
     public void save_txt(String filename) throws IOException {
-        // TODO : écrire le fichier PPM avec FileWriter
+        PrintWriter writer = new PrintWriter(new FileWriter(filename));
+        
+        writer.println("P3");
+        writer.println(width + " " + height);
+        writer.println("255");
+        for (int y = 0; y < height; y++) { 
+            for (int x = 0; x < width; x++) { 
+                for (int i = 0; i < 3; i++) {
+                    writer.print(pixels[y][x][i] + " ");
+                } 
+            }
+            writer.println();
+        }
+        writer.close();
     }
 }
