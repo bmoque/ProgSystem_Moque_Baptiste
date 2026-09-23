556d189c9a4596faa18fc26ae9d95fb32cffc58f
bmoque <baptiste.moque@iut-rodez.fr>
2026-09-23 16:26:57 +0200

    Réalisation de readInt, writeShort et readShort

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -11,23 +11,27 @@ public class Utils {
         memory[offset+2] = b1;
         memory[offset+3] = b0;
 
-        // TODO: Écrire les 4 octets de 'value' dans 'memory'
-        // à partir de 'offset', en big-endian.
         return 4;
     }
 
     public static int readInt(byte[] memory, int offset) {
-        // TODO: Reconstituer le int sur 4 octets.
-        return 0;
+        return (memory[offset] & 0xFF) << 24
+               | (memory[offset+1] & 0xFF) << 16
+               | (memory[offset+2] & 0xFF) << 8
+               | memory[offset+3]  &0xFF;
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        // TODO: Écrire les 2 octets de 'value'.
+
+        byte b1 = (byte) ((value >> 8) & 0xFF);
+        byte b0 = (byte) (value & 0xFF);
+        memory[offset] = b1;
+        memory[offset+1] = b0;
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
-        // TODO: Lire le short sur 2 octets.
-        return 0;
+        return (short) ((memory[offset] & 0xFF) << 8
+               | memory[offset+1] & 0xFF);
     }
 }
